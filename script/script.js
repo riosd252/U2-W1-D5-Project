8fc38e9044f90ae7647d274a0a74372bc9179e10
David A. Rios S. <64142938+riosd252@users.noreply.github.com>
2023-10-29 19:24:01 +0100

    Sticky navbar and tags section

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const body = document.querySelector("body");
+const navBar = document.querySelector(".nav-bar");
+const navBtn = document.querySelector(".nav-btn");
+
+window.addEventListener("scroll", (e) => {
+  console.log(body.scrollTop);
+});
